fe4b26d8fba4250205f7f75263196c31820b1550
andressa turchetto <and.turchetto@gmail.com>
2024-05-29 09:36:40 -0300

    Atualizando o projeto

--- script/main.js
+++ script/main.js
@@ -70,3 +70,10 @@ form.addEventListener("submit", (e) => {
         return false;
     }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
